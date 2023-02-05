adadba97b7be54a5d2b5d7bcf60cb8aa3e5809cf
Diego Nunes <nunesdiegond@gmail.com>
2023-02-05 10:30:29 -0300

    007 -  integrado sistema viacep para consulta automatica, está salvando endereço no banco

---
+++ Migrations/20230205131847_AtualizaProjeto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CadEleitores.Migrations
+{
+    public partial class AtualizaProjeto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
